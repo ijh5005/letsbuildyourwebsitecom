343d30a59568de65cdf4910c963ad1fed7cf9fad
Harrison <Isaiah_Harrison@comcast.com>
2018-05-16 01:55:33 -0400

    homepage animation change

--- public/js/main.js
+++ public/js/main.js
@@ -472,6 +472,39 @@ app.service('task', function($rootScope, $timeout, $interval, $http, animation, 
       data['page2Content'].push(product);
     })
   }
+
+  $timeout(() => {
+    $('.firstLift').addClass('screenLift1');
+  }, 500).then(() => {
+    $timeout(() => {
+      $('.secondLift').addClass('screenLift2');
+    }, 1000).then(() => {
+      $timeout(() => {
+        $('#screen').addClass('rotateFront');
+      }, 1000).then(() => {
+        $timeout(() => {
+          $('.firstLift').removeClass('screenLift1');
+          $('.secondLift').removeClass('screenLift2');
+        }, 1400).then(() => {
+          $timeout(() => {
+            $('#screen').addClass('backgroundColorFadeIn');
+            $('.bottomSection').addClass('shadow');
+          }, 500)
+        })
+      })
+    });
+  });
+
+  $interval(() => {
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7];
+    const random1 = Math.floor(Math.random() * numbers.length);
+    numbers.splice(numbers.indexOf(random1), 1);
+    const random2 = numbers[Math.floor(Math.random() * numbers.length)];
+    $('#screenInsideTopBottom > * > span').removeClass('lighter');
+    $(`#screenInsideTopBottom > * > span[data="${random1}"]`).addClass('lighter');
+    $(`#screenInsideTopBottom > * > span[data="${random2}"]`).addClass('lighter');
+  }, 2000)
+
 })
 
 app.service('data', function(){
